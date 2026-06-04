c28014a76e4d90fbab7686a5d893744c5131a0d9
sarthakpaul1 <sarthak.paul@mcvts.org>
2026-06-04 09:19:09 -0400

    Add files via upload

--- win_screen.gd
+++ win_screen.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,10 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	print("You win")
-	queue_free()
-	get_tree().change_scene_to_file("res://win_screen.tscn")
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
